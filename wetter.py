2983063b5636c48683268bc24a0893323ab0d8db
J. Schade <J.C.Schade@hotmail.de>
2016-08-23 15:41:07 +0200

    Function to create a list with dates

--- wetter.py
+++ wetter.py
@@ -1,22 +1,8 @@
 # -*- coding: utf-8 -*-
 import csv
+from datetime import date, datetime, timedelta
 
 # Hier werden die Werte aus der .CSV gelesen und für die weitere Verarbeitung gespeichert.
-"""def wert (datei, zeile, spalte1, spalte2):
-	liste1=[]
-	liste2=[]
-	with open (datei, 'r') as data_file:
-		reader = csv.reader(data_file, delimiter=';')
-		for row in reader:
-			liste1.append(row)
-		liste2=liste1[zeile]
-		ergebnis1 = liste2[spalte1]
-		ergebnis2 = liste2[spalte2]
-		return ergebnis1, ergebnis2
-		
-w1 = wert ("test.csv", 1, 2, 3)
-print w1"""
-
 def wert (datei, spalte1, spalte2):
 	liste0=[]
 	listespalte1=[]
@@ -34,8 +20,32 @@ def wert (datei, spalte1, spalte2):
 			listegemischt.append(element[spalte2])
 			wertedict.update({element[spalte1]:element[spalte2]})
 		#return listespalte1, listespalte2
-		return listegemischt
+		#return listegemischt
+		return wertedict
 		
 w1 = wert ("test.csv", 2, 3)
 print w1
 
+# folgedes Segment stammt von: http://stackoverflow.com/questions/10688006/generate-a-list-of-datetimes-between-an-interval abgerufen am 23.08.2016
+"""from datetime import date, datetime, timedelta
+
+def perdelta(start, end, delta):
+    curr = start
+    while curr < end:
+        yield curr
+        curr += delta
+
+for result in perdelta(date(2016, 01, 01), date(2016, 12, 31), timedelta(days=1)):
+     print result"""
+
+# Hier wird eine Liste erzeugt, in der ein bestimmtes Datumintervall generiert wird.
+def intervall(start, ende, delta):
+    while start < ende:
+        yield start
+        start += delta
+
+liste=[]
+for result in intervall(date(2016, 01, 01), date(2017, 01, 01), timedelta(days=1)):
+     liste.append(str(result))
+#     print result
+print liste
