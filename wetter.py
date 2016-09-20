ffbb62d8a7b185002adeb5bbfc05c950947f3de8
J. Schade <J.C.Schade@hotmail.de>
2016-09-20 23:32:25 +0200

    New march weather rule added

--- wetter.py
+++ wetter.py
@@ -987,6 +987,313 @@ def mae1_mae_regen(csv_werte1, csv_werte2):
 #3.Teil der Mae: 1. Regel
 # Sommersegen wie jan1_sommer_heiss
 
+#Mae: 2. Regel: Hält St. Ruprecht (28.03.) den Himmel rein, so wird es auch im Juli sein.
+
+#1.Teil der Mae: 2. Regel
+def mae2_rup_sonne(csv_werte):
+	
+#Zuerst wird das benötigte Datumintervall erezugt
+
+	def intervall(start, ende, delta):
+		while start < ende:
+			yield start
+			start += delta
+	datumliste=[]
+#	dict={}
+#	liste1=[]
+	liste2=[]
+	ergebnis=[]
+	year=1990
+	
+	ylist=[]
+	vlist=[]
+
+#In der folgenden while Schleife werden die gleichen Intervalle in unterschiedlichen Jahren max. 1990-2015 erzeugt.
+	while year<2016:
+		datumliste=[]
+		liste2=[]
+		for result in intervall(date(year, 3, 28), date(year, 3, 29), timedelta(days=1)):
+			datumliste.append(str(result))
+		for element in datumliste:
+
+#Es gibt manche Werte die nicht gemessen wurden z.B. 20.01.1993 Sonnenscheindauer
+			if element in csv_werte.iterkeys():
+#				dict.update({element:csv_werte[element]})
+#				liste1.append(element)
+				liste2.append(csv_werte[element])
+				
+#			else:
+				#pass
+
+#Hier wird das Komma in einen Punkt umgewandelt, damit später die Strings durch Floats ersetzt werden.			
+		counter=0
+		position=0
+		for element in liste2:
+			element_neu=element.replace(",", ".")
+			liste2[position]=element_neu
+			position=position+1
+
+#Hier wird gezählt, wie viele Werte den Anforderungen entsprechen.
+		position=0
+		while position < len(liste2):
+			if float(liste2[position]) > 0.0:
+				counter=counter + 1
+				position=position+1
+			else:
+				position=position+1
+#		print liste2
+
+#Nun werden in der Liste "ergebnis" das Jahr, die Anzahl an entsprechenden Werten und die max. Anzahl der möglichen Werte gespeichert.
+		ergebnis.append(year)
+		ergebnis.append(counter)
+		ergebnis.append(len(liste2))
+		
+		
+#		vergleich=0
+#		ylist.append(year)
+#		vergleich=100/len(liste2)*counter
+#		vlist.append(vergleich)
+#		print len(liste2)
+#		print counter
+#		print liste2
+		year=year+1
+	return ergebnis
+	
+#2.Teil der Mae: 2. Regel
+def mae2_juli_sonne(csv_werte):
+	
+#Zuerst wird das benötigte Datumintervall erezugt
+
+	def intervall(start, ende, delta):
+		while start < ende:
+			yield start
+			start += delta
+	datumliste=[]
+#	dict={}
+#	liste1=[]
+	liste2=[]
+	ergebnis=[]
+	year=1990
+	
+	ylist=[]
+	vlist=[]
+
+#In der folgenden while Schleife werden die gleichen Intervalle in unterschiedlichen Jahren max. 1990-2015 erzeugt.
+	while year<2016:
+		datumliste=[]
+		liste2=[]
+		for result in intervall(date(year, 7, 1), date(year, 8, 1), timedelta(days=1)):
+			datumliste.append(str(result))
+		for element in datumliste:
+
+#Es gibt manche Werte die nicht gemessen wurden z.B. 20.01.1993 Sonnenscheindauer
+			if element in csv_werte.iterkeys():
+#				dict.update({element:csv_werte[element]})
+#				liste1.append(element)
+				liste2.append(csv_werte[element])
+				
+#			else:
+				#pass
+
+#Hier wird das Komma in einen Punkt umgewandelt, damit später die Strings durch Floats ersetzt werden.			
+		counter=0
+		position=0
+		for element in liste2:
+			element_neu=element.replace(",", ".")
+			liste2[position]=element_neu
+			position=position+1
+
+#Hier wird gezählt, wie viele Werte den Anforderungen entsprechen.
+		position=0
+		while position < len(liste2):
+			if float(liste2[position]) > 0.0:
+				counter=counter + 1
+				position=position+1
+			else:
+				position=position+1
+#		print liste2
+
+#Nun werden in der Liste "ergebnis" das Jahr, die Anzahl an entsprechenden Werten und die max. Anzahl der möglichen Werte gespeichert.
+		ergebnis.append(year)
+		ergebnis.append(counter)
+		ergebnis.append(len(liste2))
+		
+		
+#		vergleich=0
+#		ylist.append(year)
+#		vergleich=100/len(liste2)*counter
+#		vlist.append(vergleich)
+#		print len(liste2)
+#		print counter
+#		print liste2
+		year=year+1
+	return ergebnis
+	
+		
+#In der Liste "ergenis" wird das ergebnis (Jahr, entsprechenede Elemente, max. Anzahl möglicher Elemente) zurück gegeben.
+	
+
+#3.Teil der Mae: 2. Regel
+def mae2_rup_regen(csv_werte1, csv_werte2):
+	
+#Zuerst wird das benötigte Datumintervall erezugt
+
+	def intervall(start, ende, delta):
+		while start < ende:
+			yield start
+			start += delta
+	datumliste=[]
+#	dict={}
+#	liste1=[]
+	ergebnis=[]
+	year=1990
+	
+	ylist=[]
+	vlist=[]
+
+#In der folgenden while Schleife werden die gleichen Intervalle in unterschiedlichen Jahren max. 1990-2015 erzeugt.
+	while year<2016:
+		datumliste=[]
+		liste2=[]
+		liste3=[]
+		for result in intervall(date(year, 3, 28), date(year, 3, 29), timedelta(days=1)):
+			datumliste.append(str(result))
+		for element in datumliste:
+
+#Es gibt manche Werte die nicht gemessen wurden z.B. 20.01.1993 Sonnenscheindauer
+			if element in csv_werte1.iterkeys():
+#				dict.update({element:csv_werte[element]})
+#				liste1.append(element)
+				liste2.append(csv_werte1[element])
+				
+		for element in datumliste:
+			if element in csv_werte2.iterkeys():
+				liste3.append(csv_werte2[element])
+				
+#			else:
+				#pass
+
+#Hier wird das Komma in einen Punkt umgewandelt, damit später die Strings durch Floats ersetzt werden.			
+		counter=0
+		position=0
+		for element in liste2:
+			element_neu=element.replace(",", ".")
+			liste2[position]=element_neu
+			position=position+1
+			
+		position=0
+		for element in liste3:
+			element_neu=element.replace(",", ".")
+			liste3[position]=element_neu
+			position=position+1
+
+#Hier wird gezählt, wie viele Werte den Anforderungen entsprechen.
+		position=0
+		while position < len(liste2):
+			if float(liste2[position]) > 0.0 and float(liste3[position]) == 0.0:
+				counter=counter + 1
+				position=position+1
+			else:
+				position=position+1
+#		print liste2
+
+#Nun werden in der Liste "ergebnis" das Jahr, die Anzahl an entsprechenden Werten und die max. Anzahl der möglichen Werte gespeichert.
+		ergebnis.append(year)
+		ergebnis.append(counter)
+		ergebnis.append(len(liste2))
+		
+		
+#		vergleich=0
+#		ylist.append(year)
+#		vergleich=100/len(liste2)*counter
+#		vlist.append(vergleich)
+#		print len(liste2)
+#		print counter
+#		print liste2
+		year=year+1
+	return ergebnis
+
+#4.Teil der Mae: 2. Regel
+def mae2_juli_regen(csv_werte1, csv_werte2):
+	
+#Zuerst wird das benötigte Datumintervall erezugt
+
+	def intervall(start, ende, delta):
+		while start < ende:
+			yield start
+			start += delta
+	datumliste=[]
+#	dict={}
+#	liste1=[]
+	ergebnis=[]
+	year=1990
+	
+	ylist=[]
+	vlist=[]
+
+#In der folgenden while Schleife werden die gleichen Intervalle in unterschiedlichen Jahren max. 1990-2015 erzeugt.
+	while year<2016:
+		datumliste=[]
+		liste2=[]
+		liste3=[]
+		for result in intervall(date(year, 7, 1), date(year, 8, 1), timedelta(days=1)):
+			datumliste.append(str(result))
+		for element in datumliste:
+
+#Es gibt manche Werte die nicht gemessen wurden z.B. 20.01.1993 Sonnenscheindauer
+			if element in csv_werte1.iterkeys():
+#				dict.update({element:csv_werte[element]})
+#				liste1.append(element)
+				liste2.append(csv_werte1[element])
+				
+		for element in datumliste:
+			if element in csv_werte2.iterkeys():
+				liste3.append(csv_werte2[element])
+				
+#			else:
+				#pass
+
+#Hier wird das Komma in einen Punkt umgewandelt, damit später die Strings durch Floats ersetzt werden.			
+		counter=0
+		position=0
+		for element in liste2:
+			element_neu=element.replace(",", ".")
+			liste2[position]=element_neu
+			position=position+1
+			
+		position=0
+		for element in liste3:
+			element_neu=element.replace(",", ".")
+			liste3[position]=element_neu
+			position=position+1
+
+#Hier wird gezählt, wie viele Werte den Anforderungen entsprechen.
+		position=0
+		while position < len(liste2):
+			if float(liste2[position]) > 0.0 and float(liste3[position]) == 0.0:
+				counter=counter + 1
+				position=position+1
+			else:
+				position=position+1
+#		print liste2
+
+#Nun werden in der Liste "ergebnis" das Jahr, die Anzahl an entsprechenden Werten und die max. Anzahl der möglichen Werte gespeichert.
+		ergebnis.append(year)
+		ergebnis.append(counter)
+		ergebnis.append(len(liste2))
+		
+		
+#		vergleich=0
+#		ylist.append(year)
+#		vergleich=100/len(liste2)*counter
+#		vlist.append(vergleich)
+#		print len(liste2)
+#		print counter
+#		print liste2
+		year=year+1
+	return ergebnis
+
+
 #Funtionen zum zeichnen der Diagramme:
 def balkendia (werte):
 	ywerte=[]
@@ -1115,6 +1422,7 @@ def adidia3 (werte1, werte2, werte3, namen, titel):
 	
 	plt.show()
 	
+	
 
 #Hier werden nun erst die "wertedict" mit den benötigten CSV-Daten erzeugt und anschließend die Funktion mit dem entsprechenden wertedict ausgeführt.
 w1 = csv_werte ("CSVs/Sonnenschein_0915.csv", 2,3)
@@ -1153,6 +1461,13 @@ w17 = csv_werte ("CSVs/Neuschnee_0915.csv", 2,3)
 print mae1_mae_regen(w16,w17)
 w18 = csv_werte ("CSVs/Lufttemp_Max_0915.csv", 2,3)	
 print jan1_sommer_heiss(w18)
+w19 = csv_werte ("CSVs/Sonnenschein_0915.csv", 2,3)
+print mae2_rup_sonne(w19)
+print mae2_juli_sonne(w19)
+w20 = csv_werte ("CSVs/Niederschlag_0915.csv", 2,3)
+w21 = csv_werte ("CSVs/Neuschnee_0915.csv", 2,3)
+print mae2_rup_regen(w20,w21)
+print mae2_juli_regen(w20,w21)
 
 
 #balkendia(jan1_sommer_heiss(w3))
@@ -1172,3 +1487,6 @@ adidia3(feb2_feb_schnee(w12),feb2_feb_eis(w13),jan1_sommer_heiss(w14), ['Feb. Sc
 
 #Mae: 1. Regel: Im März viel Schnee und Regen bringt wenig Sommersegen.
 adidia3(mae1_mae_schnee(w15),mae1_mae_regen(w16,w17),jan1_sommer_heiss(w18), ['Mae. Schnee', 'Mae. Regen', 'Sommersegen'], 'Auswertung 1. Mae. Weisheit/Regel')
+#Mae: 2. Regel: Hält St. Ruprecht (28.03.) den Himmel rein, so wird es auch im Juli sein.
+adidia2(mae2_rup_sonne(w19),mae2_juli_sonne(w19), ['St.Ruprecht (28.03.) Sonne', 'Juli Sonne'], 'Auswertung 2. Mae. Weisheit/Regel 1.Teil')
+adidia2(mae2_rup_regen(w20,w21),mae2_juli_regen(w20,w21), ['St.Ruprecht (28.03.) Regen', 'Juli Regen'], 'Auswertung 2. Mae. Weisheit/Regel 2.Teil')
